896e8766c74831fa36744222e14ff82de13abfd7
Rohit Kumar <kumar25rohit08@gmail.com>
2024-02-06 13:23:25 +0530

    adding one more file

---
+++ CodeForces/practice/topic_based/segment_trees/part1/step4/C_Number_of_Inversions_on_Segment.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C_Number_of_Inversions_on_Segment{
+  public static void main(String[] args)throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    br.close();
+  }
+}
