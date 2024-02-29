ab7de0b96757bbd50510897f5849eeae4f5e66fd
Rohit Kumar <kumar25rohit08@gmail.com>
2024-03-01 03:15:25 +0530

    learned lazy propagation

--- CodeForces/practice/topic_based/segment_trees/part2/step2/F_Assignment_and_Sum.java
+++ CodeForces/practice/topic_based/segment_trees/part2/step2/F_Assignment_and_Sum.java
@@ -2,23 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class E_Assignment_and_Minimum{
+public class F_Assignment_and_Sum{
 
   static class SegTree{
     int size;
     long[] operations;
     long[] values;
-    long NEUTRAL_ELEMENT = Long.MAX_VALUE;
+    long NEUTRAL_ELEMENT = 0;
     long NO_OPERATION = Long.MAX_VALUE - 1;
-
-    long modify_op(long a, long b){
-      if(b == NO_OPERATION) return a;
-      return b;
-    }
-
-    long calc_op(long a, long b){
-      return Math.min(a, b);
-    }
 
     void init(int n){
       size = 1;
@@ -27,13 +18,22 @@ public class E_Assignment_and_Minimum{
       values = new long[2 * size];
     }
 
+    long modify_op(long a, long b, int len){
+      if(b == NO_OPERATION) return a;
+      return b*len ;
+    }
+
+    long calc_op(long a, long b){
+      return a + b;
+    }
+
     void propagate(int x, int lx, int rx){
       if(rx - lx == 1) return;
-
-      operations[2 * x + 1] = modify_op(operations[2 * x + 1], operations[x]);
-      values[2 * x + 1] = modify_op(values[2 * x + 1], operations[x]);
-      operations[2 * x + 2] = modify_op(operations[2 * x + 2], operations[x]);
-      values[2 * x + 2] = modify_op(values[2 * x + 2], operations[x]);
+      int mid = (lx + rx) / 2;
+      operations[2 * x + 1] = modify_op(operations[2 * x + 1], operations[x], 1);
+      values[2 * x + 1] = modify_op(values[2 * x + 1], operations[x], mid - lx);
+      operations[2 * x + 2] = modify_op(operations[2 * x + 2], operations[x], 1);
+      values[2 * x + 2] = modify_op(values[2 * x + 2], operations[x], rx - mid);
 
       operations[x] = NO_OPERATION;
     }
@@ -41,9 +41,9 @@ public class E_Assignment_and_Minimum{
     void modify(int l, int r, int v, int x, int lx, int rx){
       propagate(x, lx, rx);
       if(lx >= r || rx <= l) return;
-      if(lx >= l && rx <= r){
-        operations[x] = modify_op(operations[x], v);
-        values[x] = modify_op(values[x], v);
+      else if(lx >= l && rx <= r){
+        operations[x] = modify_op(operations[x], v, 1);
+        values[x] = modify_op(values[x], v, rx - lx);
         return;
       }
 
@@ -60,7 +60,7 @@ public class E_Assignment_and_Minimum{
     long calc(int l, int r, int x, int lx, int rx){
       propagate(x, lx, rx);
       if(lx >= r || rx <= l) return NEUTRAL_ELEMENT;
-      if(lx >= l && rx <= r) return values[x];
+      else if(lx >= l && rx <= r) return values[x];
 
       int mid = (lx + rx) / 2;
       long m1 = calc(l, r, 2 * x + 1, lx, mid);
@@ -76,25 +76,28 @@ public class E_Assignment_and_Minimum{
 
   public static void main(String[] args)throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
-    int n = Integer.parseInt(input[0]);
-    int m = Integer.parseInt(input[1]);
+
+    String[] input1 = br.readLine().split(" ");
+    int n = Integer.parseInt(input1[0]);
+    int m = Integer.parseInt(input1[1]);
 
     SegTree st = new SegTree();
     st.init(n);
 
     while(m-- > 0){
-      String[] input1 = br.readLine().split(" ");
-      int type = Integer.parseInt(input1[0]);
+      String[] input2 = br.readLine().split(" ");
+      int type = Integer.parseInt(input2[0]);
+
       if(type == 1){
-        int l = Integer.parseInt(input1[1]);
-        int r = Integer.parseInt(input1[2]);
-        int v = Integer.parseInt(input1[3]);
+        int l = Integer.parseInt(input2[1]);
+        int r = Integer.parseInt(input2[2]);
+        int v = Integer.parseInt(input2[3]);
 
         st.modify(l, r, v);
       }else{
-        int l = Integer.parseInt(input1[1]);
-        int r = Integer.parseInt(input1[2]);
+        int l = Integer.parseInt(input2[1]);
+        int r = Integer.parseInt(input2[2]);
+
         System.out.println(st.calc(l, r));
       }
     }
